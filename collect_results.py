a9b5e0ce91c141116712f63abf074a95de6a0ad5
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2025-04-15 16:54:33 +0200

    Update collect_results.py

--- collect_results.py
+++ collect_results.py
@@ -5,7 +5,7 @@ results_folder = 'results/'
 results = pd.DataFrame()
 for file in os.listdir(results_folder):
     if file.endswith('.csv'):
-        df = pd.read_csv(results_folder + file, sep=';')
+        df = pd.read_csv(results_folder + file, sep=';', index_col=0)
         results = pd.concat([results, df], axis=0)
 
 print(results.mean())
