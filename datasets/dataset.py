87bd9e859b8e69a081c5fe5521e0542ed119f415
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2025-04-15 12:56:16 +0200

    exps CERES

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,12 +1,324 @@
 import os
 import pickle
 
+import torch
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder, KBinsDiscretizer, LabelEncoder, LabelBinarizer
 from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
 from .FeatureBinarizer import FeatureBinarizer
+
+import pandas as pd 
+from torch.utils.data import Dataset, DataLoader
+import re
+
+def read_keel_header(file_path):
+        '''
+        Reads the header of the provided dataset file in KEEL format.
+        The function stores the dataset parameters in the "header" attribute and the
+        position of the first line after the header in the "data_startline" attribute.
+        :param file_path: Path to the dataset file.
+        :type file_path: str
+        '''
+        attributes = {}
+        header = {
+            'relation': {},
+            'inputs': {},
+            'outputs': {}
+        }
+        with open(file_path, 'r') as f:
+            for i, line in enumerate(f):
+                if line.endswith('\n'):
+                    line = line[:-1]
+                parts = line.split(' ')
+                # Remove possible blank spaces at the end of the line
+                parts = list(filter(lambda x: x != '', parts))
+
+                if parts[0] == '@relation':
+                    header['relation'] = parts[1]
+                elif parts[0] == '@attribute' or parts[0] == '@Attribute':
+                    attr_name = parts[1]
+                    # CATEGORICAL VALUES:
+                    if '{' in attr_name:
+                        # Categorical data
+                        attr_name, values = attr_name.split('{')
+                        attributes[attr_name] = {}
+                        attributes[attr_name]['type'] = 'categorical'
+                        values[:-1].strip()  # Remove all possible blank spaces
+                        range_vals = values[:-1].split(',')
+                        attributes[attr_name]['range'] = range_vals
+                    elif len(parts) == 3 and '{' in parts[2]:
+                        # If there's an space between the attribute name and its values:
+                        attr_name = attr_name
+                        values = parts[2][1:-1]
+                        attributes[attr_name] = {}
+                        attributes[attr_name]['type'] = 'categorical'
+                        values = values.split(',')
+                        range_vals = values
+                        attributes[attr_name]['range'] = range_vals
+                    elif len(parts) > 3 and '{' in parts[2]:
+                        # If there's an space between the attribute name and its values:
+                        attr_name = attr_name
+                        values = parts[2:]
+                        attributes[attr_name] = {}
+                        attributes[attr_name]['type'] = 'categorical'
+                        values[0] = values[0].strip('{')
+                        values[-1] = values[-1].strip('}')
+                        for i in range(len(values)):
+                            values[i] = values[i].split(',')[0]
+                        range_vals = values
+                        attributes[attr_name]['range'] = range_vals
+                    # REAL VALUES:
+                    elif parts[2].startswith('real'):
+                        attributes[attr_name] = {}
+                        attributes[attr_name]['type'] = 'real'
+                        if len(parts) == 5:
+                            # Sometimes there is a blank space between real and the attributes, and others there's not
+                            range_vals = [*re.findall('\d+\.\d+', parts[3]), *re.findall('\d+\.\d+', parts[4])]
+                        else:
+                            range_vals = re.findall('\d+\.\d+', parts[2])
+                        if range_vals == []:
+                            range_vals = [-np.inf, np.inf]
+                        attributes[attr_name]['range'] = (float(range_vals[0]), float(range_vals[1]))
+                    # INTEGER VALUES:
+                    elif parts[2].startswith('integer'):
+                        attributes[attr_name] = {}
+                        attributes[attr_name]['type'] = 'integer'
+                        if len(parts) == 5:
+                            range_vals = [*re.findall(r'\d+', parts[3]), *re.findall('\d+', parts[4])]
+                        elif len(parts) == 4:
+                            range_vals = re.findall(r'\d+', parts[3])
+                        else:
+                            range_vals = re.findall(r'\d+', parts[2])
+                        attributes[attr_name]['range'] = (int(range_vals[0]), int(range_vals[1]))
+                elif parts[0] == '@inputs':
+                    for input in parts[1:]:
+                        if input.endswith(','):
+                            input = input[:-1]
+                        if input != '':
+                            # Filter possible final blank spaces
+                            header['inputs'][input] = attributes[input]
+                elif parts[0] == '@outputs' or parts[0] == '@output':
+                    for output in parts[1:]:
+                        header['outputs'][output] = attributes[output]
+                elif parts[0] == '@data':
+                    return header, i
+
+        return header, -1
+
+
+
+class auxKeelDataset(Dataset):
+
+    def __init__(self, X, y) -> None:
+        self.X = X
+        self.y = y
+        self.num_classes = len(torch.unique(y))
+        self.num_attributes = X.shape[1]
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+def transform_to_pytorch_dataset(X, y, device):
+    '''
+    Transforms the input and output data to a Pytorch dataset.
+    '''
+    X = torch.tensor(X, dtype=torch.float32)
+    y = torch.tensor(y.values, dtype=torch.long)
+
+    X = X.to(device=device)
+    y = y.to(device=device)
+
+    return auxKeelDataset(X, y)
+
+
+def read_data(file_path, class_names=None, random_state=33):
+    '''
+    Reads and returns the data of the required partition/s of the provided dataset file in KEEL format.
+
+    :param file_path: Path to the dataset file.
+    :type file_path: str
+    :class_names: Names of the classes of the dataset. If None, the class names will be inferred from the dataset.
+    :type class_names: list<str>
+    :param train: If None, all samples in the dataset will be returned.
+        If True, the first 80% examples are returned (train-val split). Otherwise, the last 20% examples are returned (test split).
+    :type train: bool
+    :param precomputed_partitions: If True, precomputed (randomly and stratified) files dataset_train.dat and dataset_test.dat will be used.
+        Otherwise, partitions will be computed from the same file as described by param "train".
+    :type precomputed_partitions: bool
+    '''
+    header, data_startline = read_keel_header(file_path)
+    if class_names is None:
+        # If class associations haven't been provided, generate them (to rename them to values from 0 to num_classes-1):
+        # Create association between dataset classes and predicted classes:
+        class_names_ = {}
+        output_attr_name = next(iter(header['outputs']))
+        for i, class_name in enumerate(header['outputs'][output_attr_name]['range']):
+            class_names_[class_name] = i
+            # Read samples from the dataset:
+    else:
+        class_names_ = class_names
+    # Read the corresponding samples:
+    data = pd.read_csv(file_path, sep=',', header=data_startline,
+                        names=(*header['inputs'].keys(), *header['outputs'].keys()), skipinitialspace=True)
+    # Remove possible blank spaces at the end of the line
+    for column in data.columns:
+        if data[column].dtype == 'object':  # Check if the column is of type object (string)
+            data[column] = data[column].map(lambda x: x.strip() if isinstance(x, str) else x)
+
+    data_input = data[[key for key in header['inputs'].keys()]]
+    # One-hot encode categorical input variables:
+    categorical_columns = [key for key in header['inputs'].keys() if header['inputs'][key]['type'] == 'categorical']
+    boolean_cateogrical_columns = [key in categorical_columns for key in header['inputs'].keys() ]
+    boolean_real_columns = np.logical_not(boolean_cateogrical_columns)
+
+    # There may be categorical variables with values that are not present in the training set:
+    # In order to ensure that pd.get_dummies creates a column for such non-existent values, we set the type of each categorical column
+    # to pd.CategoricalDtype prior to calling pd.get_dummies, specifying all potential values (even non-present ones):
+    # https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy)
+    column_types = {}
+    for column in categorical_columns:
+        comun_data, unique_classes = pd.factorize(data_input[column])
+        data_input = data_input.assign(**{column: comun_data})
+    #data_input = data_input.astype(column_types)
+    # data_input = pd.get_dummies(data_input, columns=categorical_columns)
+
+    data_target = data[header['outputs'].keys()]
+    # Rename classes to the ordered keys:
+    output_name = list(header['outputs'].keys())[0]
+    data_target = data_target[output_name].apply(lambda x: class_names_[str(x)])
+    # Standardize all columns:
+    # data_input = (data_input - data_input.mean()) / (data_input.std() + float_info.epsilon)
+    data_input = data_input.copy()
+    data_input.loc[:, boolean_real_columns] = data_input.loc[:, boolean_real_columns].astype(float)
+    data_different_min = data_input.loc[:, boolean_real_columns].min()
+    data_different_max = data_input.loc[:, boolean_real_columns].max()
+    import warnings
+    warnings.filterwarnings("ignore")
+    data_input.loc[:, boolean_real_columns] = (data_input.loc[:, boolean_real_columns] - data_different_min) / (
+        data_different_max - data_different_min)
+
+    # Code the categorical variables as integers:
+    '''from sklearn import preprocessing
+    for column in categorical_columns:
+        le = preprocessing.LabelEncoder()
+        aux = le.fit_transform(data_input.loc[:, column])
+        data_input.drop(column, axis=1, inplace=True)
+        # Change the data type of the column to the data type of aux
+        data_input[column] = aux'''
+
+
+    return data_input, data_target, header, class_names_, boolean_cateogrical_columns
+
+def load_KeelDatasetPytorch(file_path, batch_size, train_proportion=0.8, random_state=33):
+    '''
+    :param file_path: Path to the dataset file.
+    :type file_path: str
+    :param batch_size: Number of samples in each minibatch.
+    :type batch_size: int
+    :param train: If None, all samples in the dataset will be returned.
+        If True, the first 80% examples are returned (train-val split). Otherwise, the last 20% examples are returned (test split).
+    :type train: bool
+    :param train_proportion: Proportion of the dataset that will be used for training. The rest will be used for validation.
+    :type train_proportion: float
+    :param precomputed_partitions: If True, precomputed (randomly and stratified) files dataset_train.dat and dataset_test.dat will be used.
+        Otherwise, partitions will be computed from the same file as described by param "train".
+    :type precomputed_partitions: bool
+    :param num_workers: Number of workers used to load the data.
+    :type num_workers: int
+    :param pin_memory: If True, the data will be stored in the device/CUDA memory before returning them.
+    :type pin_memory: bool
+    '''
+    # 1.-Prepare the datasets:
+
+    X, y, header, class_names, boolean_categorical_vector = read_data(file_path)
+
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 - train_proportion, random_state=random_state, stratify=y)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=random_state, stratify=y_train)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test, header, boolean_categorical_vector
+
+
+
+
+def load_keel_dataset_pytorch(file_path, batch_size, train_proportion=0.8, num_workers=1, pin_memory=True, device=None, random_seed=33):
+    '''
+    :param file_path: Path to the dataset file.
+    :type file_path: str
+    :param batch_size: Number of samples in each minibatch.
+    :type batch_size: int
+    :param train: If None, all samples in the dataset will be returned.
+        If True, the first 80% examples are returned (train-val split). Otherwise, the last 20% examples are returned (test split).
+    :type train: bool
+    :param train_proportion: Proportion of the dataset that will be used for training. The rest will be used for validation.
+    :type train_proportion: float
+    :param precomputed_partitions: If True, precomputed (randomly and stratified) files dataset_train.dat and dataset_test.dat will be used.
+        Otherwise, partitions will be computed from the same file as described by param "train".
+    :type precomputed_partitions: bool
+    :param num_workers: Number of workers used to load the data.
+    :type num_workers: int
+    :param pin_memory: If True, the data will be stored in the device/CUDA memory before returning them.
+    :type pin_memory: bool
+    '''
+    X_train, X_val, X_test, y_train, y_val, y_test, header, boolean_categorical_vector = load_KeelDatasetPytorch(file_path, batch_size, train_proportion, random_state=random_seed)
+    columns = list(X_train.columns)
+    # Normalize scaling the data:
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    # Scale only the non-categorical columns:
+    boolean_categorical_vector = np.array(boolean_categorical_vector)
+    X_train = X_train.values
+    X_val = X_val.values
+    X_test = X_test.values
+
+    if sum(~boolean_categorical_vector) > 0:
+        X_train[:, ~boolean_categorical_vector] = scaler.fit_transform(X_train[:, ~boolean_categorical_vector])
+        X_val[:, ~boolean_categorical_vector] = scaler.transform(X_val[:, ~boolean_categorical_vector])
+        X_test[:, ~boolean_categorical_vector] = scaler.transform(X_test[:, ~boolean_categorical_vector])
+
+    trainDataset = transform_to_pytorch_dataset(X_train, y_train, device=device)
+    valDataset = transform_to_pytorch_dataset(X_val, y_val, device=device)
+    testDataset = transform_to_pytorch_dataset(X_test, y_test, device=device)
+
+    trainDataLoader = DataLoader(trainDataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+    valDataLoader = DataLoader(valDataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+    testDataLoader = DataLoader(testDataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+
+    return trainDataLoader, valDataLoader, testDataLoader, header, columns, boolean_categorical_vector
+
+
+def load_keel(dataset_name):
+    #file_path = os.path.join("../keel_datasets", dataset_name, dataset_name + ".dat")
+    trainDataLoader, valDataLoader, testDataLoader, auxData, feat_names, boolean_categorical_vector = load_keel_dataset_pytorch(dataset_name, batch_size=32, train_proportion=0.8, num_workers=0, pin_memory=True)
+    X_train = trainDataLoader.dataset.X.cpu().detach().numpy()
+    X_train = pd.DataFrame(X_train, columns=feat_names)
+    y_train = trainDataLoader.dataset.y.cpu().detach().numpy()
+    X_val = valDataLoader.dataset.X.cpu().detach().numpy()
+    X_val = pd.DataFrame(X_val, columns=feat_names)
+    y_val = valDataLoader.dataset.y.cpu().detach().numpy()
+    X_test = testDataLoader.dataset.X.cpu().detach().numpy()
+    X_test = pd.DataFrame(X_test, columns=feat_names)
+    y_test = testDataLoader.dataset.y.cpu().detach().numpy()
+
+    X_train = X_train.fillna(X_train.mean())
+    X_val = X_val.fillna(X_val.mean())
+    X_test = X_test.fillna(X_test.mean())
+
+    X_total = pd.concat([X_train, X_val, X_test], axis=0)
+    X_total = X_total.fillna(X_total.mean())
+    y_total = np.concatenate([y_train, y_val, y_test], axis=0)
+    
+    translator = X_total.columns
+    different_labels = np.unique(y_total)
+    label_dict = {different_labels[i]:i for i in range(len(different_labels))}
+    return X_total, y_total, label_dict, translator
+
 
 def predefined_dataset(name, binary_y=False):
     """
@@ -26,7 +338,7 @@ def predefined_dataset(name, binary_y=False):
     dir_path = os.path.dirname(os.path.realpath(__file__)) # .py
     
     ### UCI datasets
-    if name == 'adult':
+    if name == 'adultOG':
         # https://archive.ics.uci.edu/ml/datasets/adult
         # X dim: (30162, 14)
         # Y counts: {'<=50K': 22654, '>50K': 7508}
@@ -69,7 +381,11 @@ def predefined_dataset(name, binary_y=False):
         numerical_cols = None
         
     else:
-        raise NameError(f'The input dataset is not found: {name}.')
+        # It is a keel dataset
+        dataset_path = os.path.join('../keel_datasets-master/', name, name + '.dat')
+        table_X, table_Y, _, _ = load_keel(dataset_path)
+        categorical_cols = None
+        numerical_cols = None
 
     return table_X, table_Y, categorical_cols, numerical_cols
 
---
+++ mainkeel.py
@@ -0,0 +1,53 @@
+
+import torch
+import numpy as np
+import pandas as pd
+
+from datasets.dataset import transform_dataset, kfold_dataset
+from DRNet import train, DRNet
+
+
+import sys
+name = sys.argv[1]
+X, Y, X_headers, Y_headers = transform_dataset(name, method='onehot-compare', negations=False, labels='binary')
+datasets = kfold_dataset(X, Y, shuffle=1)
+X_train, X_test, Y_train, Y_test = datasets[0]
+
+train_set = torch.utils.data.TensorDataset(torch.Tensor(X_train.to_numpy()), torch.Tensor(Y_train))
+test_set = torch.utils.data.TensorDataset(torch.Tensor(X_test.to_numpy()), torch.Tensor(Y_test))
+
+
+# Train DR-Net
+# Default learning rate (1e-2), and_lam (1e-2), and and_lam (1e-5) usually work the best. A large epochs number is necessary for a sparse rule set i.e 10000 epochs.
+net = DRNet(train_set[:][0].size(1), 50, 1)
+train(net, train_set, test_set=test_set, device='cuda', lr=1e-2, epochs=1000, batch_size=10,
+      and_lam=1e-2, or_lam=1e-5, num_alter=500)
+
+# Get accuracy and the rule net
+accu = (net.predict(np.array(X_test)) == Y_test).mean()
+rules = net.get_rules(X_headers)
+print(f'Accuracy: {accu}, num rules: {len(rules)}, num conditions: {sum(map(len, rules))}')
+
+average_rule_length = np.mean(([len(rule) for rule in rules]))
+unique_conditions = set()
+for rule in rules:
+    for condition in rule:
+        if isinstance(condition, tuple):
+            condition = condition[0]
+        unique_conditions.add(condition)
+
+compelxity_score = np.log(len(rules) + len(average_rule_length) + len(unique_conditions))
+print(f'Complexity score: {compelxity_score}')
+print(f'Number of unique conditions: {len(unique_conditions)}')    
+print(f'Average rule length: {average_rule_length}')
+print(f'Number of rules: {len(rules)}')
+
+res = pd.DataFrame(
+    {'Accuracy': [accu],
+     'Complexity score': [compelxity_score],
+     'Average rule length': [average_rule_length],
+     'Number of unique conditions': [len(unique_conditions)],
+     'Number of rules': [len(rules)]},
+    index=[name])
+res.to_csv('results_' + name + '.csv', sep=';')
+
